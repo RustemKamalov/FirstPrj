aac8938574ba5b38ff4f73ece609113f4a37d07a
Rustem Kamalov <r.kamalov86@gmail.com>
2022-02-02 17:00:22 +0300

    test генератор

--- FirstPrj/First_Prj.py
+++ FirstPrj/First_Prj.py
@@ -1,61 +1,17 @@
-class multifilter:
-    def judge_half(pos, neg):
-        # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
-        return pos >= neg
-
-    def judge_any(pos, neg):
-        # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
-        return pos >= 1
-
-    def judge_all(pos, neg):
-        # допускает элемент, если его допускают все функции (neg == 0)
-        return neg == 0
-
-    def __init__(self, iterable, *funcs, judge=judge_any):
-        # iterable - исходная последовательность
-        # funcs - допускающие функции
-        # judge - решающая функция
-        self.iterator = iter(iterable)
-        self.funcs = funcs
-        self.judge = judge
-
-    def __iter__(self):
-        # возвращает итератор по результирующей последовательности
-        return self
-
-    def __next__(self):
-        while True:
-            elem = next(self.iterator)
-            pos, neg = 0, 0
-            for func in self.funcs:
-                if func(elem):
-                    pos += 1
-                else:
-                    neg += 1
-
-            if self.judge(pos, neg):
-                return elem
+import itertools
 
 
+def primes():
+    s = 1
+    lst = []
+    while True:
+        s += 1
+        for j in lst:
+            if s % j == 0:
+                break
+        else:
+            lst.append(s)
+            yield s
 
-def mul2(x):
-    return x % 2 == 0
 
-
-def mul3(x):
-    return x % 3 == 0
-
-
-def mul5(x):
-    return x % 5 == 0
-
-
-a = [i for i in range(31)] # [0, 1, 2, ... , 30]
-b = multifilter(a,  mul2, mul3, mul5, judge=multifilter.judge_all)
-c = iter(b)
-print(next(c))
-print(next(c))
-
-print(list(multifilter(a, mul2, mul3, mul5)))
-print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half)))
-print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_all)))
+print(list(itertools.takewhile(lambda x : x <= 31, primes())))
