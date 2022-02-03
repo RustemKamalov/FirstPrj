138113e8c0e8c37df684941ac8d88acdfd42e106
Rustem Kamalov <r.kamalov86@gmail.com>
2022-02-03 17:39:32 +0300

    test работа с файлами3

--- FirstPrj/First_Prj.py
+++ FirstPrj/First_Prj.py
@@ -3,8 +3,8 @@ import os.path
 
 with open("test_w.txt", "w") as out:
     for current_dir, dirs, files in os.walk("main"):
-        for _ in os.listdir(current_dir):
-            if _.endswith(".py"):
+        for file in files:
+            if file.endswith(".py"):
                 out.write(current_dir + "\n")
                 break
 
